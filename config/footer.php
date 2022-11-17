46c7177effc0af011726c50967ed9eae73b98f10
eliascastillo1982 <118423012+eliascastillo1982@users.noreply.github.com>
2022-11-16 22:10:22 -0800

    Add files via upload

---
+++ config/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer fixed-bottom">
+  <strong>Acerca de: <a href=acerca.php>MediWeb</a></strong>
+</footer>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+  <!-- Control sidebar content goes here -->
+</aside>
